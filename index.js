7f3f504d4f1777ec50e869e1efd3e49e9c1e6a41
Dekatron <onigbindeayomide@gmail.com>
2017-09-22 07:57:03 +0100

    ggdfhj

--- index.js
+++ index.js
@@ -52,7 +52,8 @@ module.exports = function(bp) {
       console.log("DECRYPTED STUFF: ", decrypted);
   bp.messenger.sendText(event.user.id, `Here is a simple password I generated for you:`);
   bp.messenger.sendText(event.user.id, generatePassword)
-  event.reply('#passwordActionText');
+  convo.say('#passwordActionText')
+  //event.reply('#passwordActionText');
   event.reply('#quickReply');
   } catch (e) {
       console.log("THE ENCRYPTION KEY IS WRONG!!");
@@ -107,7 +108,7 @@ bp.hear('QUICKREPLY.B3', (event, next) => {
   console.log(customPassword());
   bp.messenger.sendText(event.user.id, `Your custom password is:`);
   bp.messenger.sendText(event.user.id, `${customPassword()}`);
-  event.reply('#passwordActionText');
+  convo.say('#passwordActionText')
   event.reply('#quickReply');
 });
 
