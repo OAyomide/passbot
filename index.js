847b1525e29ef4e5c4aa38d16ddd1432eaa08697
Dekatron <onigbindeayomide@gmail.com>
2017-09-28 06:55:26 +0100

    eiughhhhhhhhurh

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const recast = require('recastai');
 const bcrypt = require('bcrypt-nodejs');
 const inLoop = require('botpress-hitl');
 const crypto = require('cryptojs').Crypto;
+const customPass = require ('./passwords/customPassword');
+const strongPass = require('./passwords/strongPassword')
 console.log(passwordGen(30, false));
 
 module.exports = function(bp) {
@@ -14,13 +16,7 @@ module.exports = function(bp) {
   bp.hear(/GET_STARTED|hello|hi|test|hey|holla/i, (event, next) => {
     event.reply('#welcome'); // See the file `content.yml` to see the block
     event.reply('#quickReplyFast');
-    console.log('==============================');
-    console.log('=====================================');
-    console.log('==================================');
-    console.log(`THE EVENTS STUFF IS: ${event.text}`);
-    console.log('===================================');
-    console.log('================================');
-    console.log('=================================')
+   
   })
 
   // You can also pass a matcher object to better filter events
@@ -62,245 +58,13 @@ module.exports = function(bp) {
 
 
 
-// bp.hear('QUICKREPLY.B2', (event, next) => {
-//   const 
-// })
-
-bp.hear(/QUICKREPLY.B3|QUICKREPLYFAST.B3/, (event, next) => {
-  var generatePassword = require("password-generator");
-  
-  var maxLength = 18;
-  var minLength = 12;
-  var uppercaseMinCount = 3;
-  var lowercaseMinCount = 3;
-  var numberMinCount = 2;
-  var specialMinCount = 2;
-  var UPPERCASE_RE = /([A-Z])/g;
-  var LOWERCASE_RE = /([a-z])/g;
-  var NUMBER_RE = /([\d])/g;
-  var SPECIAL_CHAR_RE = /([\?\-])/g;
-  var NON_REPEATING_CHAR_RE = /([\w\d\?\-])\1{2,}/g;
-  
-  function isStrongEnough(password) {
-   var uc = password.match(UPPERCASE_RE);
-   var lc = password.match(LOWERCASE_RE);
-   var n = password.match(NUMBER_RE);
-   var sc = password.match(SPECIAL_CHAR_RE);
-   var nr = password.match(NON_REPEATING_CHAR_RE);
-   return password.length >= minLength &&
-     !nr &&
-     uc && uc.length >= uppercaseMinCount &&
-     lc && lc.length >= lowercaseMinCount &&
-     n && n.length >= numberMinCount &&
-     sc && sc.length >= specialMinCount;
-  }
-  
-  function customPassword() {
-   var password = "";
-   var randomLength = Math.floor(Math.random() * (maxLength - minLength)) + minLength;
-   while (!isStrongEnough(password)) {
-     password = generatePassword(randomLength, false, /[\w\d\?\-]/);
-   }
-   return password;
-  }
-  
-  console.log(customPassword());
-  bp.messenger.sendText(event.user.id, `Your custom password is:`);
-  bp.messenger.sendText(event.user.id, `${customPassword()}`);
-  event.reply('#passwordActionText');
-  event.reply('#quickReply');
-});
-
-
+customPass(bp);
 
 
 /**
- * we nid a custom password
+ * we need a custom password
  */
-bp.hear(/QUICKREPLY.B4|QUICKREPLYFAST.B4/, (event, next) =>{
-  if (bp.convo.find(event)) {
-    return event.reply('#askStopConvo')
-  }
-  bp.convo.start(event, convo => {
-    convo.threads['default'].addQuestion('#askPasswordLength', [
-      {
-        pattern: /(\d+)/,
-        callback:response => {
-          // convo.say('#sendResult', {
-          //   response: response.match
-          // });
-          convo.set('minLength', response.match)
-          console.log(`THE RESPONSE IS: ${response.match}`)
-          
-         convo.switchTo('lowercaseMinCount');
-         //convo.switchTo('uppercaseMinCount');
-        // convo.switchTo('specialCharacterMin');
-         
-        
-         //convo.say(`Your lowercassMin is: ${lowercaseMinNumber}`)
-        }
-      }, {
-        default: true,
-        callback: response => {
-          convo.say('#wrongNumberInput');
-          convo.repeat()
-        }
-      }
-    ]);
-
-    convo.createThread('lowercaseMinCount')
-    convo.threads['lowercaseMinCount'].addQuestion('#lowerCaseMinPass', [
-      {
-        pattern: /(\d+)/,
-        callback: (response =>{
-          convo.set('lowercaseMin', response.match);
-          convo.switchTo('uppercaseMinCount');
-        })
-      }, {
-        default: true,
-        callback: response => {
-          convo.say('#wrongNumberInput');
-          convo.repeat()
-        }
-      }
-    ])
-
-    convo.createThread('uppercaseMinCount')
-    convo.threads['uppercaseMinCount'].addQuestion('#upperCaseMinPass', [
-      {
-        pattern: /(\d+)/,
-        callback: (response =>{
-          convo.set('uppercaseMin', response.match);
-          convo.switchTo('specialCharacterMin');
-        })
-      }, {
-        default: true,
-        callback: response => {
-          convo.say('#wrongNumberInput');
-          convo.repeat()
-        }
-      }
-    ])
-
-
-    convo.createThread('specialCharacterMin')
-    convo.threads['specialCharacterMin'].addQuestion('#specialCharMinPass', [
-      {
-        pattern: /(\d+)/,
-        callback: (response =>{
-          convo.set('special', response.match);
-          convo.switchTo('numbersMin');
-        })
-      }, {
-        default: true,
-        callback: response => {
-          convo.say('#wrongNumberInput');
-          convo.repeat()
-        }
-      }
-    ])
-
-    convo.createThread('numbersMin')
-    convo.threads['numbersMin'].addQuestion('#numberPass', [
-      {
-        pattern: /(\d+)/,
-        callback: (response =>{
-          convo.set('numbersMinCount', response.match);
-          convo.switchTo('MaxLength');
-        })
-      }, {
-        default: true,
-        callback: response => {
-          convo.say('#wrongNumberInput');
-          convo.repeat()
-        }
-      }
-    ]);
-
-    convo.createThread('MaxLength')
-    convo.threads['MaxLength'].addQuestion('#askPasswordMax', [
-      {
-        pattern: /(\d+)/,
-        callback: (response =>{
-          convo.set('maxLength', response.match);
-          convo.next()
-        })
-      }, {
-        default: true,
-        callback: response => {
-          convo.say('#wrongNumberInput');
-          convo.repeat()
-        }
-      }
-    ])
-
-
-
-    convo.on('done', ()=>{
-      const txt = txt => bp.messenger.createText(event.user.id, txt)      
-      var lowerMin = convo.get('lowercaseMin');
-      var upperMin = convo.get('uppercaseMin');
-      var specialCharMin = convo.get('special');
-      var miniLength = convo.get('minLength')
-      var numMin = convo.get('numbersMinCount');
-      var maxiLength = convo.get('maxLength')
-      // convo.say('#sendResult', {
-      //   response: lowerMin.toString(),
-      //   responseTwo: upperMin,
-      //   responseThree: specialCharMin
-      // })
-      var maxLength = parseInt(maxiLength,10);
-      var minLength = parseInt(miniLength,10);
-      var uppercaseMinCount = parseInt(upperMin,10);
-      var lowercaseMinCount = parseInt(lowerMin,10);
-      var numberMinCount = parseInt(numMin,10);;
-      var specialMinCount = parseInt(specialCharMin,10);
-      var UPPERCASE_RE = /([A-Z])/g;
-      var LOWERCASE_RE = /([a-z])/g;
-      var NUMBER_RE = /([\d])/g;
-      var SPECIAL_CHAR_RE = /([\?\-])/g;
-      var NON_REPEATING_CHAR_RE = /([\w\d\?\-])\1{2,}/g;
-      if (minLength > maxLength) {
-        bp.messenger.sendText(event.user.id, "Urh Oh🚨🚨!! The maximum length of password is lesser than the minimum length of password. Please try again 👇🏾👇");
-        convo.switchTo('default');
-        event.reply('#quickReply')
-           console.log("HOUSTON!!!!")
-      }else  {
-         function customPassword() {
-          var password = "";
-          var randomLength = Math.floor(Math.random() * (maxLength - minLength)) + minLength;
-          while (!isStrongEnough(password)) {
-            password = passwordGen(randomLength, false, /[\w\d\?\-]/);
-          }
-          return password;
-         }
-         var genPass = customPassword()
-            console.log(`GEN PASS IS: ${genPass}`)
-            convo.say('#generatedPassText');
-            convo.say(txt(genPass))
-            convo.say('#passwordActionText')
-            convo.say('#quickReply');
-         //console.log(customPassword());
-      } function isStrongEnough(password) {
-        var uc = password.match(UPPERCASE_RE);
-        var lc = password.match(LOWERCASE_RE);
-        var n = password.match(NUMBER_RE);
-        var sc = password.match(SPECIAL_CHAR_RE);
-        var nr = password.match(NON_REPEATING_CHAR_RE);
-        return password.length >= minLength &&
-          !nr &&
-          uc && uc.length >= uppercaseMinCount &&
-          lc && lc.length >= lowercaseMinCount &&
-          n && n.length >= numberMinCount &&
-          sc && sc.length >= specialMinCount;
-       }
-    })
-  })
-  
-
-})
-  
-
+strongPass(bp);
 
 /**
  * WE FOUND A PREVIOUS CONVO
---
+++ passwords/customPassword.js
@@ -0,0 +1,173 @@
+module.exports = function customPassword(bp){
+    bp.hear(/QUICKREPLY.B4|QUICKREPLYFAST.B4/, (event, next) =>{
+        if (bp.convo.find(event)) {
+          return event.reply('#askStopConvo')
+        }
+        bp.convo.start(event, convo => {
+          convo.threads['default'].addQuestion('#askPasswordLength', [
+            {
+              pattern: /(\d+)/,
+              callback:response => {
+                
+                convo.set('minLength', response.match)
+                console.log(`THE RESPONSE IS: ${response.match}`)
+                
+               convo.switchTo('lowercaseMinCount');
+               
+              }
+            }, {
+              default: true,
+              callback: response => {
+                convo.say('#wrongNumberInput');
+                convo.repeat()
+              }
+            }
+          ]);
+      
+          convo.createThread('lowercaseMinCount')
+          convo.threads['lowercaseMinCount'].addQuestion('#lowerCaseMinPass', [
+            {
+              pattern: /(\d+)/,
+              callback: (response =>{
+                convo.set('lowercaseMin', response.match);
+                convo.switchTo('uppercaseMinCount');
+              })
+            }, {
+              default: true,
+              callback: response => {
+                convo.say('#wrongNumberInput');
+                convo.repeat()
+              }
+            }
+          ])
+      
+          convo.createThread('uppercaseMinCount')
+          convo.threads['uppercaseMinCount'].addQuestion('#upperCaseMinPass', [
+            {
+              pattern: /(\d+)/,
+              callback: (response =>{
+                convo.set('uppercaseMin', response.match);
+                convo.switchTo('specialCharacterMin');
+              })
+            }, {
+              default: true,
+              callback: response => {
+                convo.say('#wrongNumberInput');
+                convo.repeat()
+              }
+            }
+          ])
+      
+      
+          convo.createThread('specialCharacterMin')
+          convo.threads['specialCharacterMin'].addQuestion('#specialCharMinPass', [
+            {
+              pattern: /(\d+)/,
+              callback: (response =>{
+                convo.set('special', response.match);
+                convo.switchTo('numbersMin');
+              })
+            }, {
+              default: true,
+              callback: response => {
+                convo.say('#wrongNumberInput');
+                convo.repeat()
+              }
+            }
+          ])
+      
+          convo.createThread('numbersMin')
+          convo.threads['numbersMin'].addQuestion('#numberPass', [
+            {
+              pattern: /(\d+)/,
+              callback: (response =>{
+                convo.set('numbersMinCount', response.match);
+                convo.switchTo('MaxLength');
+              })
+            }, {
+              default: true,
+              callback: response => {
+                convo.say('#wrongNumberInput');
+                convo.repeat()
+              }
+            }
+          ]);
+      
+          convo.createThread('MaxLength')
+          convo.threads['MaxLength'].addQuestion('#askPasswordMax', [
+            {
+              pattern: /(\d+)/,
+              callback: (response =>{
+                convo.set('maxLength', response.match);
+                convo.next()
+              })
+            }, {
+              default: true,
+              callback: response => {
+                convo.say('#wrongNumberInput');
+                convo.repeat()
+              }
+            }
+          ])
+      
+      
+      
+          convo.on('done', ()=>{
+            const txt = txt => bp.messenger.createText(event.user.id, txt)      
+            var lowerMin = convo.get('lowercaseMin');
+            var upperMin = convo.get('uppercaseMin');
+            var specialCharMin = convo.get('special');
+            var miniLength = convo.get('minLength')
+            var numMin = convo.get('numbersMinCount');
+            var maxiLength = convo.get('maxLength')
+            var maxLength = parseInt(maxiLength,10);
+            var minLength = parseInt(miniLength,10);
+            var uppercaseMinCount = parseInt(upperMin,10);
+            var lowercaseMinCount = parseInt(lowerMin,10);
+            var numberMinCount = parseInt(numMin,10);;
+            var specialMinCount = parseInt(specialCharMin,10);
+            var UPPERCASE_RE = /([A-Z])/g;
+            var LOWERCASE_RE = /([a-z])/g;
+            var NUMBER_RE = /([\d])/g;
+            var SPECIAL_CHAR_RE = /([\?\-])/g;
+            var NON_REPEATING_CHAR_RE = /([\w\d\?\-])\1{2,}/g;
+            if (minLength > maxLength) {
+              bp.messenger.sendText(event.user.id, "Urh Oh🚨🚨!! The maximum length of password is lesser than the minimum length of password. Please try again 👇🏾👇");
+              convo.switchTo('default');
+              event.reply('#quickReply')
+                 console.log("HOUSTON!!!!")
+            }else  {
+               function customPassword() {
+                var password = "";
+                var randomLength = Math.floor(Math.random() * (maxLength - minLength)) + minLength;
+                while (!isStrongEnough(password)) {
+                  password = passwordGen(randomLength, false, /[\w\d\?\-]/);
+                }
+                return password;
+               }
+               var genPass = customPassword()
+                  console.log(`GEN PASS IS: ${genPass}`)
+                  convo.say('#generatedPassText');
+                  convo.say(txt(genPass))
+                  convo.say('#passwordActionText')
+                  convo.say('#quickReply');
+               //console.log(customPassword());
+            } function isStrongEnough(password) {
+              var uc = password.match(UPPERCASE_RE);
+              var lc = password.match(LOWERCASE_RE);
+              var n = password.match(NUMBER_RE);
+              var sc = password.match(SPECIAL_CHAR_RE);
+              var nr = password.match(NON_REPEATING_CHAR_RE);
+              return password.length >= minLength &&
+                !nr &&
+                uc && uc.length >= uppercaseMinCount &&
+                lc && lc.length >= lowercaseMinCount &&
+                n && n.length >= numberMinCount &&
+                sc && sc.length >= specialMinCount;
+             }
+          })
+        })
+        
+      
+      })
+}
